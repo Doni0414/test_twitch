c99713df5417e61f50ee358a1ea64a7d38a54be3
Doni0414 <nurda.agabek@mail.ru>
2024-04-03 14:59:24 +0500

    Flip.kz

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # This is a sample Python script.
+import time
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium_stealth import stealth
 
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from seleniumwire.undetected_chromedriver.v2 import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import pickle
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi(webdriver.Chrome().capabilities)
+if __name__ == "__main__":
+    options = {}
+
+    browser = Chrome()
+    browser.get('https://gmail.com')
+
+    time.sleep(50)
+    browser.get('https://www.youtube.com')
+    pickle.dump(browser.get_cookies(), open('test_cookies.pkl', 'wb'))
+    print('Dumped Successfully')
+
+    time.sleep(10)
+    browser.close()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
